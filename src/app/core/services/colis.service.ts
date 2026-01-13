f0308e15ad3c08e8960b4d3fbaaa0703c2926f64
Hanane Oubaha <Hananeouba03@gmail.com>
2026-01-13 11:45:00 +0100

    feat: création du service de gestion des colis

---
+++ src/app/core/services/colis.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ColisService {
+  private apiUrl = 'http://localhost:8080/api/colis';
+
+  constructor(private http: HttpClient) {}
+
+  getColisById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+}
