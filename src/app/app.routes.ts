161bbb51c4013aebd05ae8493bfc3d8bc07b5770
Hanane Oubaha <Hananeouba03@gmail.com>
2026-01-14 11:30:00 +0100

    feat: configuration initiale des routes de l'application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SuiviColisComponent } from './features/suivi-colis/suivi-colis.component';
+import { LoginComponent } from './features/login/login'; // Importiha
+import { DashboardComponent } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'suivi', component: SuiviColisComponent },
+  { path: 'login', component: LoginComponent }, // <--- Zidi hadi
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'suivi', pathMatch: 'full' }
+];
