400509a9e7ab2c10f999c36b3d06e7cc7327f847
Hanane Oubaha <Hananeouba03@gmail.com>
2026-01-14 14:00:00 +0100

    feat: mise à jour du composant principal de l'application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm mb-4">
+      <div class="container">
+        <a class="navbar-brand fw-bold" routerLink="/">SmartLogi <span class="text-warning">SDMS</span></a>
+        <div class="navbar-nav">
+          <a class="nav-link active" routerLink="/suivi">Suivi Colis</a>
+          </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `
 })
 export class App {
-  protected readonly title = signal('SmartLogiSdms-Front');
+  title = 'SmartLogi-Front';
 }
